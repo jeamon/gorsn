ef125b66c383345e26d9e21183e6dbed0cd76fca
Jerome Amon <aaboujerome@gmail.com>
2023-10-14 22:46:07 +0200

    re-structure the examples folders and add example to demonstrate usage of default options

---
+++ examples/custom-options/example.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"regexp"
+	"syscall"
+
+	"github.com/jeamon/gorsn"
+)
+
+func main() {
+	// step 1. define a path to a valid folder.
+	root, err := os.Getwd() // lets use this package folder
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// step 2. define own custom options.
+	excludeRule := regexp.MustCompile(`.*(\.git).*`)
+	// exclude `.git` folder. No include rule.
+	opts := gorsn.RegexOpts(excludeRule, nil).
+		// 5 events can wait into the queue.
+		SetQueueSize(5).
+		// 2 goroutines to build & emit events.
+		SetMaxWorkers(2).
+		// 0 sec - immediate, so no delay to scan.
+		SetScanInterval(0)
+		// others options keep their default values.
+
+	// step 3. get an instance based on above settings.
+	sn, err := gorsn.New(root, opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// [optional] stop the scan notifier on CTRL+C.
+	go func() {
+		sigChan := make(chan os.Signal, 1)
+		signal.Notify(sigChan, syscall.SIGINT, syscall.SIGQUIT,
+			syscall.SIGTERM, syscall.SIGHUP, os.Interrupt)
+
+		<-sigChan
+		signal.Stop(sigChan)
+		sn.Stop()
+	}()
+
+	// step 4. asynchronously receive events from the queue.
+	go func() {
+		for event := range sn.Queue() {
+			log.Printf("received %q %s %s %v\n", event.Path, event.Type, event.Name, event.Error)
+		}
+	}()
+
+	// step 5. start the scan notifier on the defined path.
+	// this line blocks unless it fails or explicitly stopped.
+	err = sn.Start(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+}
--- examples/default-options/example.go
+++ examples/default-options/example.go
@@ -5,8 +5,8 @@ import (
 	"log"
 	"os"
 	"os/signal"
-	"regexp"
 	"syscall"
+	"time"
 
 	"github.com/jeamon/gorsn"
 )
@@ -18,20 +18,11 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// step 2. set some options.
-	excludeRule := regexp.MustCompile(`.*(\.git).*`)
-	// exclude `.git` folder. No include rule.
-	opts := gorsn.RegexOpts(excludeRule, nil).
-		// 5 events can wait into the queue.
-		SetQueueSize(5).
-		// 2 goroutines to build & emit events.
-		SetMaxWorkers(2).
-		// 0 sec - immediate, so no delay to scan.
-		SetScanInterval(0)
-		// others options keep their default values.
+	// step 2. use default options.
+	var opts gorsn.Options
 
-	// step 3. get an instance based on above settings.
-	sn, err := gorsn.New(root, opts)
+	// step 3. get an instance.
+	sn, err := gorsn.New(root, &opts)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -54,10 +45,20 @@ func main() {
 		}
 	}()
 
+	done := make(chan struct{})
 	// step 5. start the scan notifier on the defined path.
-	// blocks unless it fails or until stopped.
-	err = sn.Start(context.Background())
-	if err != nil {
-		log.Fatal(err)
-	}
+	// lets run it as goroutine to bypass the blocking behavior.
+	go func() {
+		err = sn.Start(context.Background())
+		if err != nil {
+			log.Fatal(err)
+		}
+		done <- struct{}{}
+	}()
+
+	// lets change the settings on the fly.
+	opts.SetMaxWorkers(10).SetScanInterval(500 * time.Millisecond)
+
+	// wait the runner to exit
+	<-done
 }
